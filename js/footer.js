8df487a671d9e15fd7a8043d7849d413cebc9bd0
WingedMenace <ivo_el1@hotmail.com>
2024-04-18 15:50:34 -0300

    Change JS

--- js/footer.js
+++
@@ -1,7 +0,0 @@
-document.write(`<footer class="footer">
-Lorem ipsum dolor sit amet.
-<a href="mailto:mail@bue.gob.ar">Contacto</a>
-</footer>
-`;
-    
-)
--- js/nav.js
+++
@@ -1,9 +0,0 @@
-document.write(` 
-    <nav class="nav">
-          <a href="index.html">Home</a>
-            <a href="about.html">Sobre nosotros</a>
-            <a href="form.html">Contáctanos</a>
-            <a href="">Link de prueba</a>
-            </nav>
-        `;
-)
