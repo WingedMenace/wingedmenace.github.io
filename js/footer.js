9e3d1747bef7b93b07540a384e8b2d98166dc8c2
WingedMenace <ivo_el1@hotmail.com>
2024-04-18 15:36:35 -0300

    Revert changes

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,7 @@
-document.write{`
+document.write(`<footer class="footer">
 Lorem ipsum dolor sit amet.
 <a href="mailto:mail@bue.gob.ar">Contacto</a>
+</footer>
 `;
     
-}
+)
--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,9 @@
-document.write{
-    `       <a href="index.html">Home</a>
+document.write(` 
+    <nav class="nav">
+          <a href="index.html">Home</a>
             <a href="about.html">Sobre nosotros</a>
             <a href="form.html">Contáctanos</a>
             <a href="">Link de prueba</a>
+            </nav>
         `;
-}
+)
