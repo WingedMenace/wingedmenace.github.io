a40e3fe43a93d9f0384406cca91e5d80ab2b09d0
WingedMenace <ivo_el1@hotmail.com>
2024-04-18 16:45:55 -0300

    Fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.getElementById("footer").innerHTML = `Lorem ipsum dolor sit amet.
 <a href="mailto:mail@bue.gob.ar">Contacto</a>`;
 
-document.getElementById("nav").innerHTML = `<a href="index.html">Home</a>
+document.getElementsByClassName("nav").innerHTML = `<a href="index.html">Home</a>
 <a href="about.html">Sobre nosotros</a>
 <a href="form.html">Contáctanos</a>
 <a href="">Link de prueba</a>`;
