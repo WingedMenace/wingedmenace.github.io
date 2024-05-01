2446075ce8da3aa7a3b9832d1b18f7b8707e9fe4
WingedMenace <ivo_el1@hotmail.com>
2024-05-01 16:30:10 -0300

    Formatting

--- js/main.js
+++ js/main.js
@@ -15,11 +15,10 @@ document.getElementById("nav").innerHTML = `
 function userpopup() {
     var popup = document.querySelector("uPopup");
     popup.classList.toggle("show");
-  }
+}
 
 //Validar form
-function validar() 
-{
+function validar() {
     let name = document.getElementById("name");
     let clave = document.getElementById("clave");
     let comment = document.getElementById("comment")
@@ -30,22 +29,22 @@ function validar()
         document.getElementById("validar_usuario").innerHTML = "Deje su nombre";
         error = true;
         name.focus();
-       
+
     }
     if (mail.value == "") {
         document.getElementById("validar_clave").innerHTML = "No olvide su mail";
         error = true;
         mail.focus();
-     
+
     }
     if (comment.value.length <= 20) {
         document.getElementById("validar_comentario").innerHTML = "¡Su comentario es muy corto!";
         error = true;
         xommwnr.focus();
-     
+
     }
     if (error == false) {
-        
+
         document.getElementById("usuario").value = ""
         document.getElementById("validar_usuario").innerHTML = "&nbsp;";
         document.getElementById("clave").value = ""
