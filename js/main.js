4ac71b8ef92c1a295702d23769c56697d0366883
WingedMenace <ivo_el1@hotmail.com>
2024-06-30 18:59:24 -0300

    API remota

--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@ function validar() {
   return !error
 }
 
-//Consumo de API en VUE
+/*Consumo de API local - VUE
 const { createApp } = Vue
 createApp({
   data() {
@@ -131,4 +131,77 @@ createApp({
   created() {
     this.fetchData(this.url)
   }
-}).mount('#app')
+}).mount('#app')*/
+
+//API reomta
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        api:[],
+        url:'https://wingedmenace.pythonanywhere.com/api',
+        error:false,
+        cargando:true,
+        /*atributos para el guardar los valores del formulario */
+        id: 0,
+        carta: "",
+        significado: "",
+        imagen: "",
+        precio: 0,
+        stock: 0,
+    }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        eliminar(id) {
+            const url = this.url+'/' + id;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+			 alert('Registro Eliminado')
+                    location.reload(); // recarga el json luego de eliminado el registro
+                })
+        },
+        grabar(){
+            let producto = {
+                carta: this.carta,
+                significado: this.significado,
+                imagen: this.imagen,
+                precio: this.precio,
+                stock: this.stock,
+            }
+            var options = {
+                body:JSON.stringify(producto),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro grabado")
+                    window.location.href = "./productos.html";  // recarga productos.html
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Grabar")  // puedo mostrar el error tambien
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
